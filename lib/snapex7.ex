1472d2101957e324d3c7b37078163d1b360da7fa
Adrian Rangel <adrian.rangel@gmail.com>
2018-08-01 11:59:27 -0500

    genesis commit

---
+++ lib/snapex7.ex
@@ -0,0 +1,18 @@
+defmodule Snapex7 do
+  @moduledoc """
+  Documentation for Snapex7.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Snapex7.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/snapex7_test.exs
@@ -0,0 +1,8 @@
+defmodule Snapex7Test do
+  use ExUnit.Case
+  doctest Snapex7
+
+  test "greets the world" do
+    assert Snapex7.hello() == :world
+  end
+end
