d95d43221ab395acaa4fc08ff2ed602e11cc6b80
alde103 <aldebaran_103@hotmail.com>
2019-03-05 18:39:22 -0600

    Makefile and mix.exs update

--- mix.exs
+++ mix.exs
@@ -4,17 +4,78 @@ defmodule Snapex7.MixProject do
   def project do
     [
       app: :snapex7,
-      version: "0.1.1",
-      elixir: "~> 1.7.2",
-      start_permanent: Mix.env() == :prod,
-      compilers: [:elixir_make] ++ Mix.compilers(),
-      make_executable: "make",
-      deps: deps(),
+      version: "0.1.2",
+      elixir: "~> 1.8",
       name: "Snapex7",
       description: description(),
       package: package(),
-      source_url: "https://github.com/valiot/snapex7"
+      source_url: "https://github.com/valiot/snapex7",
+      compilers: [:elixir_make] ++ Mix.compilers(),
+      make_targets: ["all"],
+      make_clean: ["clean"],
+      make_executable: make_executable(),
+      make_makefile: "src/Makefile",
+      make_error_message: make_error_message(),
+      make_env: &make_env/0,
+      docs: [extras: ["README.md"], main: "readme"],
+      start_permanent: Mix.env() == :prod,
+      build_embedded: true,
+
+      deps: deps()
     ]
+  end
+
+  defp make_env() do
+    base =
+      Mix.Project.compile_path()
+      |> Path.join("..")
+      |> Path.expand()
+
+    %{
+      "MIX_ENV" => to_string(Mix.env()),
+      "PREFIX" => Path.join(base, "priv"),
+      "BUILD" => Path.join(base, "obj")
+    }
+    |> Map.merge(ei_env())
+  end
+
+  defp ei_env() do
+    case System.get_env("ERL_EI_INCLUDE_DIR") do
+      nil ->
+        %{
+          "ERL_EI_INCLUDE_DIR" => "#{:code.root_dir()}/usr/include",
+          "ERL_EI_LIBDIR" => "#{:code.root_dir()}/usr/lib"
+        }
+
+      _ ->
+        %{}
+    end
+  end
+
+  defp make_executable do
+    case :os.type() do
+      {:win32, _} ->
+        "mingw32-make"
+
+      _ ->
+        :default
+    end
+  end
+
+  @windows_mingw_error_msg """
+  You may need to install mingw-w64 and make sure that it is in your PATH. Test this by
+  running `gcc --version` on the command line.
+  If you do not have mingw-w64, one method to install it is by using
+  Chocolatey. See http://chocolatey.org to install Chocolatey and run the
+  following from and command prompt with administrative privileges:
+  `choco install mingw`
+  """
+
+  defp make_error_message do
+    case :os.type() do
+      {:win32, _} -> @windows_mingw_error_msg
+      _ -> :default
+    end
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -33,6 +94,7 @@ defmodule Snapex7.MixProject do
       files: [
         "lib",
         "src/*.[ch]",
+        "src/Makefile",
         "src/snap7/src",
         "src/snap7/build",
         "src/snap7/examples/plain-c/*.h",
@@ -51,7 +113,7 @@ defmodule Snapex7.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:elixir_make, "~> 0.4", runtime: false},
+      {:elixir_make, "~> 0.5", runtime: false},
       {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
